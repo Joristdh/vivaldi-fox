1a69fb914a5661692312b3c50d6c254ff8737d56
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-06-06 18:16:07 +0000

    Change theme with system

--- background/addon-state.js
+++ background/addon-state.js
@@ -5,91 +5,22 @@
 class AddonState {
   constructor({
     onTabColorChange,
-    onFaviconChange,
     onWindowFocusChange,
-    onNightMode,
     onInit
   }) {
-    this.state = {
-      tabColorMap: new Map(),
-      contentScript: null,
-    };
-
     onTabColorChange = onTabColorChange.bind(this);
     onWindowFocusChange = onWindowFocusChange.bind(this);
-    onNightMode = onNightMode.bind(this);
     onInit = onInit.bind(this);
 
-    const findAndSetColor = async (tab) => {
-      let baseColor = await findBaseColor(tab);
-      let color = false;
-      if (baseColor) {
-        color = new Color(baseColor);
-      }
-      this.state.tabColorMap.set(tab.id, color);
-    };
+    browser.windows.onFocusChanged.addListener(onWindowFocusChange);
+    browser.tabs.onActivated.addListener(async ({ tabId }) => onTabColorChange(await browser.tabs.get(tabId)));
+    browser.tabs.onUpdated.addListener(async (_, { status }, tab) => {
+      if (status != "loading" && tab.active) onTabColorChange(tab)
+    }, { properties: ["status"] });
 
-    browser.runtime.onMessage.addListener(async (message, {tab}) => {
-      let usePageDefinedColors = await Settings.getUsePageDefinedColors();
-      let hasPageDefinedColor = false;
-
-      let color = false;
-
-      if (usePageDefinedColors) {
-        if (message.command === "color") {
-          color = new Color(message.value);
-          hasPageDefinedColor = true;
-        } else if (message.command === "reset") {
-          color = false;
-        }
-      }
-
-      if (!hasPageDefinedColor) {
-        let baseColor = await findBaseColor(tab);
-        if (baseColor) {
-          color = new Color(baseColor);
-        }
-      }
-
-      this.state.tabColorMap.set(tab.id, color);
-
-      if (tab.active) {
-        onTabColorChange(tab);
-      }
-    });
-
-    browser.tabs.onActivated.addListener(async ({ tabId }) => {
-      let tab = await browser.tabs.get(tabId);
-      if (!this.state.tabColorMap.has(tabId)) {
-        await findAndSetColor(tab);
-      }
-      onTabColorChange(tab);
-    });
-
-    browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-      if (changeInfo.status == "loading") {
-        return;
-      }
-
-      if (changeInfo.favIconUrl) {
-        onFaviconChange(tab);
-      }
-
-      await findAndSetColor(tab);
-      if (tab.active) {
-        onTabColorChange(tab);
-      }
-    }, {properties: ["favIconUrl", "status"]});
-
-    browser.tabs.onRemoved.addListener((tabId) =>
-      this.state.tabColorMap.delete(tabId));
 
     this.refreshAddon = async () => {
       onInit();
-
-      browser.alarms.create(
-        "nightToggle", { when: (await firstAlarm()) }
-      );
 
       await new Promise(r => setTimeout(r, 400));
 
@@ -99,57 +30,12 @@ class AddonState {
       }
       for (let tab of tabs) {
         if (tab.active) {
-          let color = await findBaseColor(tab);
-          this.state.tabColorMap.set(tab.id, color);
           onTabColorChange(tab);
         }
       }
     };
 
     Settings.onChanged(this.refreshAddon);
-    browser.windows.onFocusChanged.addListener(onWindowFocusChange);
-
-    (async () => {
-      browser.alarms.create(
-        "nightToggle", { when: (await firstAlarm()) }
-      );
-    })();
-    browser.alarms.onAlarm.addListener(async ({name}) => {
-      if (name === "nightToggle") {
-        onNightMode();
-        browser.alarms.create(
-          "nightToggle", { when: (await firstAlarm()) }
-        );
-      }
-    });
     onInit();
   }
-}
-
-async function firstAlarm() {
-  let now = new Date(Date.now());
-  let then = new Date(Date.now());
-  let nightModeStart = await Settings.getNightModeStart();
-  let nightModeEnd = await Settings.getNightModeEnd();
-
-  if (nightModeStart > nightModeEnd) {
-    if (now.getHours() >= nightModeStart) {
-      then.setHours(24 + nightModeEnd);
-    } else if (now.getHours() < nightModeEnd) {
-      then.setHours(nightModeEnd);
-    } else {
-      then.setHours(nightModeStart);
-    }
-  } else {
-    if (now.getHours() >= nightModeEnd) {
-      then.setHours(24 + nightModeStart);
-    } else if (now.getHours() < nightModeStart) {
-      then.setHours(nightModeStart);
-    } else {
-      then.setHours(nightModeEnd);
-    }
-  }
-  then.setMinutes(0);
-  then.setSeconds(0);
-  return then.getTime();
 }
--- background/background.js
+++ background/background.js
@@ -2,34 +2,20 @@
 
 let currentTheme;
 
-let whiteFaviconCache = new Map();
-
-async function setColor({ id, windowId }, tabColorMap) {
+async function setColor({ windowId }) {
   let color
   try {
-    color = (await browser.tabs.executeScript({ code: `document.querySelector('meta[name="theme-color"]')?.content` }))[0]
+    if (!(color = (await browser.tabs.executeScript({ code: `document.querySelector('meta[media="(prefers-color-scheme: dark)"]')?.content` }))[0]))
+      color = (await browser.tabs.executeScript({ code: `document.querySelector('meta[name="theme-color"]')?.content` }))[0]
   } catch { }
   let win = await browser.windows.get(windowId);
   let pageColorsOnInactive = await Settings.getPageColorsOnInactive();
 
-  if (!color || color == "#ffffff" || (!pageColorsOnInactive && !win.focused)) {
+
+  if (!color || color.match(/#f.f.f./) || (!pageColorsOnInactive && !win.focused)) {
     currentTheme.reset(windowId);
   } else {
-    currentTheme.patch(color.toString(), "rgb(255,255,255)", windowId);
-  }
-}
-
-async function isDayMode() {
-  let date = new Date(Date.now());
-  let nightModeStart = await Settings.getNightModeStart();
-  let nightModeEnd = await Settings.getNightModeEnd();
-
-  if (nightModeStart > nightModeEnd) {
-    return date.getHours() >= nightModeEnd &&
-      date.getHours() < nightModeStart;
-  } else {
-    return date.getHours() < nightModeStart ||
-      date.getHours() >= nightModeEnd;
+    currentTheme.patch(color.toString(), "", windowId);
   }
 }
 
@@ -37,102 +23,31 @@ new AddonState({
   async onInit() {
     let themes = await Settings.getThemes();
 
-    let selectedTheme;
-    if (await isDayMode()) {
-      selectedTheme = await Settings.getDefaultTheme();
-    } else {
-      selectedTheme = await Settings.getNightTheme();
-    }
+    currentTheme = new Theme(themes[window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light']);
 
-    currentTheme = new Theme(themes[selectedTheme]);
+    window.matchMedia('(prefers-color-scheme: dark)').onchange = ({ matches }) => currentTheme = new Theme(themes[matches ? 'dark' : 'light']);
 
-    let hasAllUrlsPermissions = await browser.permissions.contains({
-      origins: ["<all_urls>"]
-    });
     let isFirstRun = await Settings.getIsFirstRun();
-    if (isFirstRun && !hasAllUrlsPermissions) {
+    if (isFirstRun && !await browser.permissions.contains({ origins: ["<all_urls>"] })) {
       chrome.tabs.create({
         url: chrome.runtime.getURL("/welcome/welcome.html")
       });
     }
-    if (isFirstRun)
-      Settings.setIsFirstRun(false);
-
-    let usePageDefinedColors = await Settings.getUsePageDefinedColors();
-    if (usePageDefinedColors && this.state.contentScript) {
-      this.state.contentScript = await browser.contentScripts.register({
-        matches: ["<all_urls>"],
-        js: [{ file: "data/contentscript.js" }],
-      });
-    } else if (!usePageDefinedColors && this.state.contentScript !== null) {
-      this.state.contentScript.unregister();
-      this.state.contentScript = null;
-    }
+    if (isFirstRun) Settings.setIsFirstRun(false);
   },
   onTabColorChange(tab) {
-    return setColor(tab, this.state.tabColorMap);
-  },
-  async onNightMode() {
-    let defaultTheme = await Settings.getDefaultTheme();
-    let nightTheme = await Settings.getNightTheme();
-    if (defaultTheme !== nightTheme) {
-      await this.refreshAddon();
-    }
+    return setColor(tab);
   },
   async onWindowFocusChange(focusedWindowId) {
     let tabs = await browser.tabs.query({ active: true });
-    if (tabs.length == 0) {
-      return;
-    }
+    if (tabs.length == 0) return
+
     for (let tab of tabs) {
-      if (tab.windowId == focusedWindowId) {
-        setColor(tab, this.state.tabColorMap);
-      }
+      if (tab.windowId == focusedWindowId) setColor(tab)
     }
-  },
-  async onFaviconChange(tab) {
-    let whiteFaviconsEnabled = await Settings.getWhiteBackgroundFavicons();
-    if (!whiteFaviconsEnabled) {
-      return;
-    }
-
-    let whiteFavicon;
-    if (whiteFaviconCache.has(tab.favIconUrl)) {
-      whiteFavicon = whiteFaviconCache.get(tab.favIconUrl);
-    } else {
-      whiteFavicon = await getReadableFavicon(tab.favIconUrl);
-    }
-
-    if (whiteFavicon) {
-      whiteFaviconCache.set(tab.favIconUrl, whiteFavicon);
-    }
-
-    if (!whiteFavicon) {
-      return;
-    }
-    await browser.tabs.executeScript(tab.id, {
-      code: `
-          for (let icon of document.querySelectorAll("link[rel~=icon]")) {
-            icon.remove();
-          }
-          document.head.insertAdjacentHTML(\'beforeend\', \'<link rel="icon" href="${whiteFavicon}">\');
-          `
-    });
   }
 });
 
 browser.browserAction.onClicked.addListener(() => {
   browser.runtime.openOptionsPage();
 });
-
-// Using the browser's theme color scheme with this extension interacts poorly with webpages
-// See https://github.com/nt1m/vivaldi-fox/issues/120
-async function setOverrideContentColorSchemeSetting() {
-  let { value, levelOfControl } = await browser.browserSettings.overrideContentColorScheme.get({});
-
-  if (levelOfControl != "not_controllable" && value == "browser") {
-    browser.browserSettings.overrideContentColorScheme.set({ value: "system" });
-  }
-}
-
-setOverrideContentColorSchemeSetting();
